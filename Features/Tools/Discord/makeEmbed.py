5c28228a97844c117be3bc550bc0f2647455ab1a
Mateus Campos <perdidogmaerthree@gmail.com>
2022-06-13 23:29:23 -0300

    Correction: "getMoment" function and code

--- Features/Tools/Discord/makeEmbed.py
+++ Features/Tools/Discord/makeEmbed.py
@@ -5,30 +5,32 @@ from discord import Embed, Colour
 
 
 async def makeEmbed(
-    _Title:str,                      # Needed.
-    _Description:str,                # Needed.
+    _Title:str,                         # Needed.
+    _Description:str,                   # Needed.
     _Author_Name:str = '',
     _Author_Url:str = '',
     _Author_Icon:str = '',
     _Color:Colour = Colour.blurple(),
-    _Fields:list = None,             # Must be validate every time
-    _Footer_Text:str = await getMoment(_IsoFormat=False),
-    _Footer_IconUrl:str = '',      # The URL of the footer icon. Only HTTP(S) is supported.
+    _Fields:list = None,                # Must be validate every time
+    _Footer_Text:str = '',
+    _Footer_IconUrl:str = '',           # The URL of the footer icon. Only HTTP(S) is supported.
     _Image:str = '',
-    _Provider = '',                # What is this? https://discordpy.readthedocs.io/en/stable/api.html#embed
+    _Provider = '',                     # What is this? https://discordpy.readthedocs.io/en/stable/api.html#embed
     _Thumbnail:str = '',
-    _Timestamp:datetime = datetime.today(),
-    _Type = '',                    # It will be deprecated
+    _Timestamp:datetime = None,
+    _Type = '',                         # It will be deprecated
     _Url:str = '',
-    _Video:str = ''                # Video isn't the same as Image??
+    _Video:str = ''                     # Video isn't the same as Image??
 ) -> Embed:
 
-    newEmbed = Embed(title=_Title, description=_Description, url=_Url, timestamp=_Timestamp, colour=_Color)
+    newEmbed = Embed(title=_Title, description=_Description, url=_Url, colour=_Color)
     if await validateEmbedFields(_Fields) == True:
         for field in _Fields:
             if field != None : newEmbed.add_field(name = field[0], value = field[1], inline = field[2])
     if _Author_Name != None : newEmbed.set_author(name = _Author_Name, url = _Author_Url, icon_url = _Author_Icon)
-    if _Footer_Text != None : newEmbed.set_footer(text = _Footer_Text, icon_url = _Footer_IconUrl)
+    if _Footer_Text == '' and _Footer_IconUrl == '': newEmbed.set_footer(text = await getMoment(_IsoFormat=False))
+    else: newEmbed.set_footer(text = _Footer_Text, icon_url = _Footer_IconUrl)
     if _Image != None : newEmbed.set_image(url = _Image)
     if _Thumbnail != None : newEmbed.set_thumbnail(url = _Thumbnail)
+    if _Timestamp != None : newEmbed.set_thumbnail(timestamp=_Timestamp)
     return newEmbed
--- Features/Tools/Utils/getMoment.py
+++ Features/Tools/Utils/getMoment.py
@@ -1,9 +1,15 @@
 from datetime import datetime
 
 
-async def getMoment(_IsoFormat = True) -> str:
+async def getMoment(_IsoFormat:bool = True, _KindOfSeparatorDate:str = '.', _KindOfSeparatorTime:str = ':', _WithHours:bool = True) -> str:
+    KosD = _KindOfSeparatorDate
+    KosT = _KindOfSeparatorTime
     if _IsoFormat:
         datetimeFormated = datetime.now().isoformat()
+    elif _WithHours:
+        Format = '%d{0}%b{1}%Y, %H{2}%M{3}%S'.format(KosD, KosD, KosT, KosT)
+        datetimeFormated = datetime.now().strftime(Format)
     else:
-        datetimeFormated = datetime.now().strftime("%d.%b.%Y, %H:%M:%S")
+        Format = '%d{0}%b{1}%Y'.format(KosD, KosD)
+        datetimeFormated = datetime.now().strftime(Format)
     return datetimeFormated
