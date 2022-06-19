34f5a050c701b672bd05a18f7af7c0eb7c402ceb
Mateus Campos <perdidogmaerthree@gmail.com>
2022-06-19 20:56:31 -0300

    Update: implemented Kick and Warn functions

--- Features/Tools/Discord/warnUser.py
+++ Features/Tools/Discord/warnUser.py
@@ -5,38 +5,37 @@ from Features.Tools.Discord.makeEmbed import makeEmbed
 from Features.Tools.Utils.getMoment import getMoment
 
 
-async def banUser(_Context:Context, _Member:Member, _Reason:str, _LogChannel:Messageable) -> None:
+async def warnUser(_Context:Context, _Member:Member, _Reason:str, _LogChannel:Messageable) -> None:
     # Check if it is a use for tests purposes
     IsARegularCall = _Reason.find('DDD021') == -1
     if IsARegularCall:
         # Check if the member has the permissions to invoke that command
         CanBanOrKickMembers:Permissions = _Context.author.permissions_in(channel=_Context.channel)
-        CanBanOrKickMembers:bool = CanBanOrKickMembers.ban_members or CanBanOrKickMembers.kick_members
+        CanBanOrKickMembers:bool = CanBanOrKickMembers.kick_members or CanBanOrKickMembers.kick_members
         if CanBanOrKickMembers != True:
             await _Context.send('Me respeita, rapá! Você não pode fazer isso, quero ver estalar dois dedos na tua oreia.')
             return
         
-        # Check if the member to be banned is the same as the administrator and has the rights permissions
+        # Check if the member to be kicked is the same as the administrator and has the rights permissions
         CanBanOrKickMembers:Permissions = _Member.permissions_in(channel=_Context.channel)
-        CanBanOrKickMembers:bool = CanBanOrKickMembers.ban_members or CanBanOrKickMembers.kick_members
+        CanBanOrKickMembers:bool = CanBanOrKickMembers.kick_members or CanBanOrKickMembers.kick_members
         if CanBanOrKickMembers or _Member == _Context.author:
-            await _Context.send('You can\' ban yourself or people with kick / ban permissions')
+            await _Context.send('You can\' warn yourself or people with kick / ban permissions')
             return
-        
     
-    BannedMessage = ':bat: RIP :bat:' + f'\n<@{_Member.id}>' + ' foi banido!'
+    
+    WarnedMessage = ':bat: :bat: :bat:' + f'\n<@{_Member.id}>' + ' foi avisado!'
     Date = await getMoment(_IsoFormat=False, _KindOfSeparatorDate="/", _WithHours=False)
     Reason = f'**({Date}) - {_Reason}**'
-    NewEmbed = await makeEmbed(
-        _Title='The ban hammer has spoken',
-        _Description=BannedMessage,
+    newEmbed = await makeEmbed(
+        _Title='The Bruce Wayne has spoken',
+        _Description=WarnedMessage,
         _Author_Name='Robin do Hood',
         _Author_Icon='https://cdn.discordapp.com/attachments/981836514579329064/986079281975267338/unknown.png',
         _Fields=[
             ['Motivo', Reason, False]
         ]
     )
-    await _LogChannel.send(embed=NewEmbed)
-    await _Member.send(content='Toma jeito agora, né.', embed=NewEmbed)
-    if IsARegularCall : await _Member.ban()
+    await _LogChannel.send(embed=newEmbed)
+    await _Member.send(content='', embed=newEmbed)
     return
