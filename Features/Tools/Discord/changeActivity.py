b682e00b7e1ed82799fdc47b4b3e12fb72879240
Mateus Campos <perdidogmaerthree@gmail.com>
2022-06-19 21:15:49 -0300

    Update: ChangeActivity with custom status

--- Features/Tools/Discord/changeActivity.py
+++ Features/Tools/Discord/changeActivity.py
@@ -1,13 +1,15 @@
 from itertools import cycle
 import discord
+from discord.ext.commands.context import Context
 from discord.ext import commands
 from discord.abc import Messageable
 
 
-async def changeActivity(_Client:commands.Bot, _ListOf_Status:cycle, _LogChannel:Messageable) -> None:
-    # Check current Activity
-    currentActivity = _Client.activity
-    newActivity = next(_ListOf_Status)
+async def changeActivity(_Client:commands.Bot, _Ctx:Context, _NewActivity:str, _ListOf_Status:cycle, _LogChannel:Messageable) -> None:
+    # Check if the change is for the content of the message
+    if _NewActivity != None : newActivity = _NewActivity
+    else : newActivity = next(_ListOf_Status)
+
     await _Client.change_presence(
         activity=discord.Game(newActivity)
     )
